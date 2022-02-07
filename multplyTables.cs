f0d7167299120dece28cf13d66c14ce9be63a1dc
TousifRSH <95872574+TousifRSH@users.noreply.github.com>
2022-02-08 00:29:00 +0530

    Add files via upload

---
+++ multplyTables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace FinalAsignement
+{
+    class multplyTables
+    {
+        static void Main()
+        {
+            int a = 1;
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (a <= 10)
+            {
+                Console.WriteLine(n + "*" + a + " = " + (n * a));
+                a++;
+            }
+        }
+
+}
